68d8b427f1c7321d7268048ed925b3f34d206cc7
Keith Harrison <khninethree@gmail.com>
2019-06-06 14:28:58 -0400

    Updated links to honor event modifiers.

--- src/Link.js
+++ src/Link.js
@@ -1,11 +1,36 @@
 import { RouterContext } from './RouterContext'
 
-export function Link({ to: url, children, ...props }) {
-	return (
-		<RouterContext.Consumer>
-			{({ router }) => {
-				return <a href={url} onClick={router.handle.bind(null, url)} {...props}>{children}</a>
-			}}
-		</RouterContext.Consumer>
-	)
+export class Link extends React.Component {
+
+	isModifiedEvent(event) {
+		return !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
+	}
+
+	handleClick(event, context, props) {
+		if (
+			!event.defaultPrevented
+			&& event.button === 0
+			&& (!this.props.target || this.props.target === "_self")
+			&& !this.isModifiedEvent(event)
+		) {
+			event.preventDefault()
+
+			context.router.update(this.props.to)
+		}
+	}
+
+	render() {
+		const { to: url, children, ...props } = this.props
+
+		return (
+			<RouterContext.Consumer>
+				{context => {
+					return <a href={url} onClick={event => this.handleClick(event, context)} {...props}>
+						{children}
+					</a>
+				}}
+			</RouterContext.Consumer>
+		)
+	}
+
 }
